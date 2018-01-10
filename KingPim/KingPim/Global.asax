4263ba916a1b8e4630b7f6148af36898efd833a3
David Söderberg <davviidsoderberg@outlook.com>
2018-01-10 13:30:55 +0100

    Projekt Start

---
+++ KingPim/KingPim/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KingPim.MvcApplication" Language="C#" %>
---
+++ KingPim/KingPim/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KingPim.Startup))]
+namespace KingPim
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
