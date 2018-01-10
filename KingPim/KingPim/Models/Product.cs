0923e262264a2517fdf62121ddbfbb49dac2a8e3
David Söderberg <davviidsoderberg@outlook.com>
2018-01-10 13:35:25 +0100

    Datamodell Classerna

---
+++ KingPim/KingPim/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KingPim.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public virtual Subcategory Subcategory { get; set; }
+        public int SubcategoryId { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
